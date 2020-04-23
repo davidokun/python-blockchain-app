cf3fea38c80f8c910cbd1d01bbed28fed9993f55
davidokun <davidokun99@gmail.com>
2020-04-23 11:07:42 +0200

    Extracting last value separately

--- blockchain_app/blockchain.py
+++ blockchain_app/blockchain.py
@@ -1,15 +1,23 @@
 """
 Blockchain service
 """
-BLOCK_CHAIN = [[]]
+BLOCK_CHAIN = [[0]]
 
 
-def add_value(new_value):
+def get_last_block_chain_value():
+    """
+    Retrieve last value in the blockchain
+    :return: last value of the blockchain list
+    """
+    return BLOCK_CHAIN[-1]
+
+
+def add_value(transaction_amount):
     """
     Add new value to the blockchain list
-    :param new_value: the new value
+    :param transaction_amount: the new value
     """
-    BLOCK_CHAIN.append([BLOCK_CHAIN[-1], new_value])
+    BLOCK_CHAIN.append([get_last_block_chain_value(), transaction_amount])
     print(BLOCK_CHAIN)
 
 
