267cbb1cd6e946b613b7926cf4352d68d2e56b58
davidokun <davidokun99@gmail.com>
2020-04-23 10:23:39 +0200

    Lists

---
+++ basics/lists.py
@@ -0,0 +1,17 @@
+my_list = [1, 'Hi', 2.54]
+print(my_list)
+
+print(my_list[1])
+
+my_list.append(3)
+print(my_list)
+
+print(my_list.pop())
+print(my_list)
+
+
+def my_function():
+    print('Hallo')
+
+
+my_function()
