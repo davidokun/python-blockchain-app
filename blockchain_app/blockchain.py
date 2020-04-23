de6d7a0d52dc473a8e9af10c2892ee90d3404646
davidokun <davidokun99@gmail.com>
2020-04-23 11:39:16 +0200

    Adding docs and refactors

--- blockchain_app/blockchain.py
+++ blockchain_app/blockchain.py
@@ -7,23 +7,28 @@ BLOCK_CHAIN = [[0]]
 def get_last_block_chain_value():
     """
     Retrieve last value in the blockchain
-    :return: last value of the blockchain list
+
+    :return: last value on the blockchain list
     """
     return BLOCK_CHAIN[-1]
 
 
 def add_value(transaction_amount):
     """
-    Add new value to the blockchain list
-    :param transaction_amount: the new value
+    Add new value to add in the blockchain list.
+    New value will be a new list with size 2 composed by last_value and transaction_amount
+
+    :param transaction_amount: amount of the transaction to be added to the blockchain
     """
     BLOCK_CHAIN.append([get_last_block_chain_value(), transaction_amount])
 
 
-NUM_OF_TX = int(input('How many transaction do you want: '))
+if __name__ == '__main__':
+    NUM_OF_TX = int(input('How many transaction do you want?: '))
 
-for i in range(NUM_OF_TX):
-    tx_amount = input(f'Type the amount for transaction No {i + 1} please: ')
-    add_value(float(tx_amount))
+    for i in range(NUM_OF_TX):
+        tx_number = i + 1
+        tx_amount = input(f'Type the amount for transaction No {tx_number} please: ')
+        add_value(float(tx_amount))
 
-print(BLOCK_CHAIN)
+    print(BLOCK_CHAIN)
