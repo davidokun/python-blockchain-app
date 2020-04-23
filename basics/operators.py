52036f9757a01bf5f9a90c97fc44f2cb5d3f77a6
davidokun <davidokun99@gmail.com>
2020-04-23 10:13:23 +0200

    Operators

---
+++ basics/operators.py
@@ -0,0 +1,13 @@
+print('=' * 30)
+
+float_result = 5 / 10
+print(float_result)
+
+integer_result = 5 // 10
+print(integer_result)
+
+hello = 'Guten ' + 'Abend'
+print(hello)
+
+print(1e10)
+print(1 - 0.9)
